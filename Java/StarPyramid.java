a1727ae3381a5502aa1c90a41de014faf63f9dcd
Malsha Samarakoon <chamodi96samarakoon@gmail.com>
2021-10-09 14:28:55 +0530

    half-pyramid added

---
+++ Java/StarPyramid.java
@@ -0,0 +1,21 @@
+
+public class StarPyramid{
+
+      public static void main(String args[]){
+        
+              for(int x=1; x<=5; x++) {     
+               
+                    for(int y=1; y<=x; y++) {     
+                          System.out.print("*");
+                    } 
+                 System.out.println();        
+              }      
+        System.out.println();     
+  
+        }
+  }
+  
+
+
+
+
