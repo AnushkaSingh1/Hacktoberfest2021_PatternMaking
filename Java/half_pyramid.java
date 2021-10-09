0dce5ea98f1cb9dc182be019805a69a5244705b6
Malsha Samarakoon <chamodi96samarakoon@gmail.com>
2021-10-09 14:18:38 +0530

    star pattern

---
+++ Java/half_pyramid.java
@@ -0,0 +1,26 @@
+
+public class Ten{
+
+    public static void main(String args[]){
+
+      System.out.println( );
+      
+      //Pattern No.3
+            for(int x=1; x<=5; x++) {     
+             
+                  for(int y=1; y<=x; y++) {     
+                        System.out.print("*");
+                  } 
+               System.out.println();        
+            }      
+      System.out.println();     
+
+      
+      System.out.println();
+      }
+}
+
+
+
+
+
